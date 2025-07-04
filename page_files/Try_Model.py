f25c567af69bc8d14b94c2b8ea58b64aa04b7b83
Shail K Patel <114606340+ShailKPatel@users.noreply.github.com>
2025-07-05 01:57:51 +0530

    Delete page_files directory

--- page_files/Try_Model.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-
-# Logo
-st.logo("resources/logo.png", icon_image="resources/logo.png",size='large')
-
-# Title
-st.title("Test the model yourself!")
-
--- page_files/View_Model_Analysis.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-
-# Logo
-st.logo("resources/logo.png", icon_image="resources/logo.png",size='large')
-
