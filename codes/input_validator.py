be0376fe0829e42ac4523c567019c47a5c5c90be
Shail K Patel <114606340+ShailKPatel@users.noreply.github.com>
2025-07-05 01:57:59 +0530

    Delete codes directory

--- codes/input_validator.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import numpy as np
-
-def validate_string(input_string):
-    if not (input_string.strip() or input_string==""):
-        st.error("The text input is empty. Please enter some text.")
